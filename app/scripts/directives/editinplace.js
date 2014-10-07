25dbc86525e383f44e810f82415deae8684af297
Konviser <svetsapelova@gmail.com>
2014-10-07 17:50:24 +0200

    add images to the post

--- app/scripts/directives/editinplace.js
+++ app/scripts/directives/editinplace.js
@@ -3,10 +3,14 @@
 angular.module('tumblrcrawlerApp')
   .directive('editInPlace', function($compile){
 
-    var dateTemplate = '<span data-ng-click="edit()" ng-bind="value | date:\'medium\'"></span>'+
-                       '<input class="form-control" type="text" data-ng-model="value" value="value"></input>';
-    var titleTemplate = '<span data-ng-click="edit()" ng-bind="value"></span>'+
-                       '<input class="form-control" type="text" data-ng-model="value" value="value"></input>';
+    var dateTemplate = '<span data-ng-click="edit()" ng-bind="value.timestamp | date:\'medium\'"></span>'+
+                       '<input class="form-control" type="text" data-ng-model="value.timestamp" value="value.timestamp"></input>';
+    var titleTemplate = '<span data-ng-click="edit()" ng-bind="value.title"></span>'+
+                       '<input class="form-control" type="text" data-ng-model="value.title" value="value.title"></input>';
+
+    var imageTemplate = '<span data-ng-click="edit()">Add Images</span>'+
+                        '<input class="form-control" data-ng-keydown="addImages($event)" data-ng-model="image" type="text" placeholder="type image URL and press Enter"></input>';
+
 
     var getTemplate = function(type){
       var template = '';
@@ -16,6 +20,9 @@ angular.module('tumblrcrawlerApp')
           break;
         case 'title':
           template = titleTemplate;
+          break;
+        case 'img':
+          template = imageTemplate;
           break;
       }
       return template;
@@ -35,10 +42,19 @@ angular.module('tumblrcrawlerApp')
         inputElement[0].focus();
       }
 
+      scope.addImages = function($event){
+        if ($event.keyCode === 13) {
+          scope.value.imgs.push(scope.image);
+          scope.editing = false;
+          element.removeClass('active');
+        }
+      };
+
       inputElement.on('blur', function () {
         scope.editing = false;
-        element.removeClass('active')
+        element.removeClass('active');
       });
+
 
     };
 
